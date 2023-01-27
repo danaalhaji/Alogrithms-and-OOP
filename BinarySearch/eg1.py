eba2d6c9122260a7a2d7dd3bd5f7b984adda3f61
Dana Alhaji <dana.alhaji95@gmail.com>
2023-01-27 16:57:12 +0200

    fix middle

--- BinarySearch/eg1.py
+++ BinarySearch/eg1.py
@@ -1,8 +1,11 @@
+import math
+
+
 def binary_search(list, item):
     low = 0
     high = len(list)-1
     while low <= high:
-        mid = low+high
+        mid = math.floor((low+high)/2)
         guess = list[mid]
         if item == guess:
             return mid
